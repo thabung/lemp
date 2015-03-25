ce2cf50c8e0f87475722ca99b8417a0aa5ba08ba
thabung <thabungm@gmail.com>
2015-03-25 18:44:07 +0530

    Update lemp.sh
    adding the commands

---
+++ lemp.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install mysql-server php5-mysql
+sudo mysql_install_db
+sudo /usr/bin/mysql_secure_installation
+
+echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+sudo apt-get update
+sudo apt-get install nginx
+
+sudo apt-get install php5-fpm
